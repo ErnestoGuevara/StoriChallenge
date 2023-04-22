2a9450703373c247fa4e20749676ef84ad69a52e
Ernesto Guevara <neto_1208@hotmail.com>
2023-04-22 00:03:03 -0600

    email validator

--- cmd/app/emailSender/emailSender.go
+++ cmd/app/emailSender/emailSender.go
@@ -3,6 +3,7 @@ package emailSender
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/ErnestoGuevara/StoriChallenge/cmd/app/config"
 	"github.com/ErnestoGuevara/StoriChallenge/cmd/app/logger"
@@ -21,42 +22,50 @@ func SendEmail(balance string, averageCredit string, averageDebit string, transa
 	}
 
 	from := mail.NewEmail("Stori Challenge", "neto120899@hotmail.com")
-
-	to := mail.NewEmail("Client", os.Getenv("EMAIL_ADDRESS"))
-
-	message := mail.NewV3Mail()
-	message.SetFrom(from)
-	message.Subject = "Summary Report"
-
-	personalization := mail.NewPersonalization()
-	personalization.AddTos(to)
-	personalization.Subject = "Summary Report"
-
-	personalization.SetDynamicTemplateData("balance", balance)
-	personalization.SetDynamicTemplateData("debitavg", averageDebit)
-	personalization.SetDynamicTemplateData("creditavg", averageCredit)
-
-	var transactions []model.Transaction
-
-	for month, numTran := range transactionsMonthly {
-		transactions = append(transactions, model.Transaction{
-			Month:   month,
-			NumTran: numTran,
-		})
-	}
-	personalization.SetDynamicTemplateData("transactionsMonthly", transactions)
-	message.AddPersonalizations(personalization)
-	message.SetTemplateID(cfg.SendGrid.Template)
-
-	client := sendgrid.NewSendClient(cfg.SendGrid.Api)
-	response, err := client.Send(message)
-	if err != nil {
+	emailAddress := os.Getenv("EMAIL_ADDRESS")
+	//Validate if it is a valid email address
+	if !strings.Contains(emailAddress, "@") {
+		// Handle the error case
 		logger := logger.NewLogger("EMAIL_ERROR: ")
-		logger.Error(fmt.Sprintf("Error consuming email API: %v", err))
+		logger.Error("¡Invalid email address!")
+	} else {
+		to := mail.NewEmail("Client", os.Getenv("EMAIL_ADDRESS"))
 
-	} else if response.StatusCode == 202 {
-		logger := logger.NewLogger("EMAIL_INFO: ")
-		logger.Info("¡Email sended!")
+		message := mail.NewV3Mail()
+		message.SetFrom(from)
+		message.Subject = "Summary Report"
+
+		personalization := mail.NewPersonalization()
+		personalization.AddTos(to)
+		personalization.Subject = "Summary Report"
+
+		personalization.SetDynamicTemplateData("balance", balance)
+		personalization.SetDynamicTemplateData("debitavg", averageDebit)
+		personalization.SetDynamicTemplateData("creditavg", averageCredit)
+
+		var transactions []model.Transaction
+
+		for month, numTran := range transactionsMonthly {
+			transactions = append(transactions, model.Transaction{
+				Month:   month,
+				NumTran: numTran,
+			})
+		}
+		personalization.SetDynamicTemplateData("transactionsMonthly", transactions)
+		message.AddPersonalizations(personalization)
+		message.SetTemplateID(cfg.SendGrid.Template)
+
+		client := sendgrid.NewSendClient(cfg.SendGrid.Api)
+		response, err := client.Send(message)
+		if err != nil {
+			logger := logger.NewLogger("EMAIL_ERROR: ")
+			logger.Error(fmt.Sprintf("Error consuming email API: %v", err))
+
+		} else if response.StatusCode == 202 {
+			logger := logger.NewLogger("EMAIL_INFO: ")
+			logger.Info("¡Email sended!")
+		}
+
 	}
 
 	return nil
